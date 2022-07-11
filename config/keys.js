143db2ef39776e22cb0f698fd4b14bde355ee21e
krutiman <awreyhgsrf@gmail.com>
2022-07-11 10:59:56 +0300

    Registration + Auth

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://krutiman:den04032002@cluster0.1qiod5d.mongodb.net/?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
+}
---
+++ controllers/auth.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.login = async function(req, res){
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 3600})
+            
+            res.status(200).json({
+                token: `Bearer ${token}`
+            });
+        } else {
+            res.status(401).json ({
+                message: 'Wrong password'
+            });
+        }
+
+    } else {
+        res.status(404).json({
+            message: `The user with email ${req.body.email} was not found`
+        });
+    }
+}
+
+module.exports.register = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        res.status(409).json({
+            message: 'This email has already been registered, try another one'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch(e) {
+            errorHandler(res, e);
+        }
+    }
+};
